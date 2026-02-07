e4f837ee2a51f98e350e57f8990b80d0f983d733
kkprobe <ypr.ribeiro@gmail.com>
2026-02-07 18:03:02 +0000

    mudançasmenu bruh

--- menu/menu.py
+++ menu/menu.py
@@ -1,16 +1,31 @@
 lista_num = []
 #-----variaveis----global-----#
-numero = 0
-media = 0
-soma = 0
 
 
 
-print("------MENU------")
-print("1- Incluir número na lista")
-print("2- Remover número da lista")
-print("3- Calcular média da lista")
-print("4- Calcular somatório")
-print("5- Imprimir lista")
+while True :
+    print("------MENU------")
+    print("1- Incluir número na lista")
+    print("2- Remover número da lista")
+    print("3- Calcular média da lista")
+    print("4- Calcular somatório")
+    print("5- Imprimir lista")
+    print("6- Sair")
 
-num_usuario = int(input(""))
+    opcao_usuario = int(input(""))
+
+    if opcao_usuario == 1:
+
+    elif opcao_usuario == 2:
+
+    elif opcao_usuario == 3:
+
+    elif opcao_usuario == 4:
+
+    elif opcao_usuario == 5:
+
+    elif opcao_usuario == 6:
+        break
+
+    else:
+        print("Essa opção não existe.")
