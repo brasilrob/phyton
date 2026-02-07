6aa21f2f0781e9aecae5fd8bc2e744e9cc7cf7fe
kkprobe <ypr.ribeiro@gmail.com>
2026-02-07 17:53:51 +0000

    menu

---
+++ menu/menu.py
@@ -0,0 +1,13 @@
+lista_num = []
+
+
+
+
+print("------MENU------")
+print("1- Incluir número na lista")
+print("2- Remover número da lista")
+print("3- Calcular média da lista")
+print("4- Calcular somatório")
+print("5- Imprimir lista")
+
+num_usuario = int(input(""))
