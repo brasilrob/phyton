47cdd3dd02ed8b3e1bb3458416918a6aed500c26
gabriebbrasil2017@gmail.com <gabriebbrasil2017@gmail.com>
2026-02-07 18:18:53 +0000

    media002

---
+++ menu/Calcuramedia.py
@@ -0,0 +1,9 @@
+def media (*args):
+    if len(args) == 0:
+        return "Nenhum número fornecido."
+    
+    total = sum(args)
+    count = len(args)
+    average = total / count
+    
+    return f"A média dos números fornecidos é: {average}"
